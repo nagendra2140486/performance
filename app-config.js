e05b7a50105186630535ade3072ef21efdff574f
nagendra2140486 <163975570+nagendra2140486@users.noreply.github.com>
2026-09-01 15:02:34 +0530

    Create app-config.js

---
+++ app-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'voyagenie': {
+    repo: 'https://github.com/nagendra2140486/voyagenie.git',
+    branch: 'nagendra/functionaltests',
+    perfScript: 'perf/critical-journeys.js',
+    reportScript: 'perf/generate-performance-report.js'
+  },
+
+  'timesheet-app': {
+    repo: 'https://github.com/nagendra2140486/timesheet-app.git',
+    branch: 'main',
+    perfScript: 'perf/critical-journeys.js',
+    reportScript: 'perf/generate-performance-report.js'
+  }
+};
